f59d07f60dfa2ba303c1ce55a75ab934f65bdcac
celineboudaie <cboudaie@gmail.com>
2018-07-13 18:32:21 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,18 @@
+class Song
 
+ attr_accessor :name
+ attr_accessor :artist
+ attr_accessor :genre
+ 
+  def initialize(name, artist, genre)
+@name= name
+@artist= artist
+@genre= genre 
+@@song_count= 0
+@@song_count += 1
+end
+def self.song_count
+  @@song_count
+end
+end
+Song.song_count
